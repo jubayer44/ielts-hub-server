09fa2b00307e37e7052bd0cba68316c8e9fd85a1
jubayer44 <jubayerahmedshamim44@gmail.com>
2022-11-08 19:59:58 +0600

    server-upload-on-vercel

--- index.js
+++ index.js
@@ -13,7 +13,20 @@ const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zihtklt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function connectDb(){
+    try {
+        const serviceCollection = client.db('IELTS-Hub').collection('services');
+        const reviewCollection = client.db('IELTS-Hub').collection('review');
 
+        app.get('/data', (req, res)  => {
+            res.send('database connection successful')
+        })
+    }
+    finally {
+
+    }
+}
+connectDb().catch(err => console.log(err));
 
 
 app.get('/', (req, res) => {
