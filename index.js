b830ac0e7a0eb489e910c974711007907a0bff6f
jubayer44 <jubayerahmedshamim44@gmail.com>
2022-11-08 19:38:19 +0600

    server-connect-with-mongodb

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zihtklt.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('IELTS Server is running')
+});
+
+app.listen(port, () =>{
+    console.log(`server running on port ${port}`);
+})
