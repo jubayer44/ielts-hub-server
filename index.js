a06db89b2be600a978359833dbd3f0cfe247c3a1
jubayer44 <jubayerahmedshamim44@gmail.com>
2022-11-08 23:53:38 +0600

    serviceDetails-routes-added

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zihtklt.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -18,16 +18,26 @@ async function connectDb(){
         const serviceCollection = client.db('IELTS-Hub').collection('services');
         const reviewCollection = client.db('IELTS-Hub').collection('review');
 
+        //Routes for Home page services
         app.get('/', async (req, res) => {
             results = await serviceCollection.find({}).limit(3).toArray();
             res.send(results);
-
         });
 
         app.get('/services', async (req, res)  => {
             const services = await serviceCollection.find({}).toArray();
             res.send(services)
-        })
+        });
+
+        app.get('/services/:id', async (req, res) => {
+            const {id} = req.params;
+            const service = await serviceCollection.findOne({_id: ObjectId(id)});
+            res.send(service);
+        });
+
+
+
+
     }
     finally {
 
