7b49ffbf977e5d1f6b2e09b29ed3628d12132e57
jubayer44 <jubayerahmedshamim44@gmail.com>
2022-11-09 12:06:49 +0600

    create-review-routes-for-each-services

--- index.js
+++ index.js
@@ -1,62 +1,64 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-
-
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.zihtklt.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
 
-async function connectDb(){
-    try {
-        const serviceCollection = client.db('IELTS-Hub').collection('services');
-        const reviewCollection = client.db('IELTS-Hub').collection('review');
+async function connectDb() {
+  try {
+    const serviceCollection = client.db("IELTS-Hub").collection("services");
+    const reviewCollection = client.db("IELTS-Hub").collection("review");
 
-        //Routes for Home page services
-        app.get('/', async (req, res) => {
-            results = await serviceCollection.find({}).limit(3).toArray();
-            res.send(results);
-        });
+    //Routes for Home page services
+    app.get("/", async (req, res) => {
+      results = await serviceCollection.find({}).limit(3).toArray();
+      res.send(results);
+    });
 
-        app.get('/services', async (req, res)  => {
-            const services = await serviceCollection.find({}).toArray();
-            res.send(services)
-        });
+    app.get("/services", async (req, res) => {
+      const services = await serviceCollection.find({}).toArray();
+      res.send(services);
+    });
 
-        app.get('/services/:id', async (req, res) => {
-            const {id} = req.params;
-            const service = await serviceCollection.findOne({_id: ObjectId(id)});
-            res.send(service);
-        });
+    app.get("/services/:id", async (req, res) => {
+      const { id } = req.params;
 
-        //Reviews Routes
-        app.post('/reviews', async (req, res) => {
-            const review = req.body;
-            const newReview = await reviewCollection.insertOne(review);
-            res.send(newReview)
-            console.log(newReview);
-        });
+      const service = await serviceCollection.findOne({ _id: ObjectId(id) });
+      res.send(service);
+    });
 
-        app.get('/reviews', async (req, res) => {
-            const results = await reviewCollection.find({}).toArray();
-            res.send(results);
-        });
+    //Reviews Routes
+    app.get("/reviews/:id", async (req, res) => {
+      const { id } = req.params;
+      const results = await reviewCollection.find({ serviceId: id }).toArray();
+      res.send(results);
+    });
+
+    app.post("/reviews/", async (req, res) => {
+        const review = req.body;
+        const newReview = await reviewCollection.insertOne(review);
+        res.send(newReview);
+      });
 
 
-    }
-    finally {
 
-    }
+
+  } finally {
+  }
 }
-connectDb().catch(err => console.log(err));
+connectDb().catch((err) => console.log(err));
 
-
-app.listen(port, () =>{
-    console.log(`server running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`server running on port ${port}`);
+});
